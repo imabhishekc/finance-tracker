04a2da5b87c59f539c6e9da3d7bd9bb478e13dfe
imabhishekc <abhishekchakraborty776@gmail.com>
2025-05-08 13:14:17 +0545

    Initial commit - Finance Tracker

---
+++ main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import csv
+from datetime import datetime
+from data_entry import get_date, get_amount, get_category, get_description
+
+class CSV:
+    CSV_FILE = "finance_data.csv"
+    COLUMNS = ["date", "amount", "category", "description"]
+
+    @classmethod
+    def initialize_csv(cls):
+        try:
+            pd.read_csv(cls.CSV_FILE)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=cls.COLUMNS)
+            df.to_csv(cls.CSV_FILE, index=False)
+
+    @classmethod
+    def add_entry(cls, date, amount, category, description):
+        new_entry = {
+            "date": date,
+            "amount": amount,
+            "category": category,
+            "description": description
+        }
+        with open(cls.CSV_FILE, "a", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
+            writer.writerow(new_entry)
+            print("Entry added successfully")
+
+
+
+CSV.initialize_csv()
+CSV.add_entry("2025-05-01", "1000", "Income", "Salary")
